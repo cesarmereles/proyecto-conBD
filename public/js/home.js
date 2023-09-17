4b2dc5b66dd21d802d3a54f17ea83398c06f947b
Cesar Augusto Mereles <cesarmereles@gmail.com>
2023-09-17 17:35:18 -0300

    renderizado app.js y boton para mostrar datos de la BD

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,6 @@
 
 const btnUsers = document.querySelector('.btn-get-users');
+const btnPublicaciones = document.querySelector('.btn-get-publicaciones');
 
 
 
@@ -30,9 +31,36 @@ btnUsers.addEventListener('click', async () => {
     const response = await fetch('/obtener-usuarios');
     const users = await response.json();
     const tablaUsuarios = document.querySelector('#tabla-usuarios');
-    
-
     // Modificar el DOM para mostrar los datos
     mostrarDatos(users, tablaUsuarios)
+})
 
+//=========================================================================
+const mostrarPublicaciones = (publicacion, elemento) => {
+    let registros = '';
+    publicacion.forEach(user => {
+        registros += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.titulo}</td>
+                <td>${user.detalle}</td>
+                <td>${user.url_imagen}</td>
+                <td>${user.fecha_publicacion}</td>
+            </tr>
+        `
+    });
+
+    elemento.innerHTML = registros
+}
+
+
+
+
+btnPublicaciones.addEventListener('click', async () => {
+    // Consulta al servidor por los datos de usuarios
+    const response = await fetch('/listado-de-publicaciones');
+    const publicacion = await response.json();
+    const tablaPublicaciones = document.querySelector('#tabla-publicaciones');
+    // Modificar el DOM para mostrar los datos
+    mostrarPublicaciones(publicacion, tablaPublicaciones)
 })
