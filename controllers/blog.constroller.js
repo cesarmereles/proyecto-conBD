37f63ed8c0ceac20bc900d6bf31eaaa01272d451
Cesar Augusto Mereles <cesarmereles@gmail.com>
2023-09-23 20:53:07 -0300

    crud completo

--- controllers/blog.constroller.js
+++ controllers/blog.constroller.js
@@ -1,14 +1,17 @@
 const Publicacion = require('../models/Publicaciones')
+//TODO INCLUYO TODA LA FUNCIONALIDAD DENTRO DE UN OBJETO ASI EXPORTO EL OBJETO PADRE
 const ctrol = {}
 
 
 ctrol.newPost = async (req, res)=> {
-    const {titulo,detalle,url_imagen,fecha_publicacion} = req.body;
+    // const {titulo,detalle,url_imagen,fecha_publicacion} = req.body;
     try {
       //!aca creamos la instancia del registro con el modelo de datos
       const publicaciones2 = await Publicacion.create({
         titulo,detalle,url_imagen,fecha_publicacion
       })
+      //!await publicaciones2.save() <- esto guarda la instancia que guardamos con sequelize
+      await publicaciones2.save()
       res.send({msg:'Publicación creada con exito',publicaciones2})  
     } catch (error) {
       console.log('Error es: ',error)
@@ -29,6 +32,49 @@ ctrol.getPosts = async (req, res)=>{
         msg:'Error al consultar las Publicaciones'
       })  
     }
-  }
+}
 
-  module.exports = ctrol
+ctrol.updatePosts = async (req, res)=>{
+  const {id} = req.params
+  try {
+    const publicacion = await Publicacion.findByPk(id)
+    //!antes de setear esos valores hay que tener varias validaciones
+    publicacion.set(req.body)
+
+    //!ahora guardo en la base de datos
+    await publicacion.save();
+    return res.json({
+      msg:'Publicación actualizada correctamente'
+    })
+
+
+  } catch (error) {
+    console.log('Error es: ',error)
+      return res.status(500).json({
+        msg:'Error al actualizar la Publicacion'
+      })  
+  }
+}
+
+ctrol.deletePosts = async (req,res)=>{
+  const {id} = req.params;;
+  try {
+    await Publicacion.destroy({
+      where:{
+        id
+      }
+    })
+    return res.json({
+      msg: 'Publicacion eliminada exitosamente'
+    })
+  } catch (error) {
+    console.log('Error es: ',error)
+    return res.status(500).json({
+      msg:'Error al eliminar la Publicacion'
+    })  
+  }
+}
+
+
+//!EXPORTO EL OBJETO PADRE
+module.exports = ctrol
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,44 +1,53 @@
 //*USAMOS EL ENRUTADOR DE LA LIBRERIA EXPRESS
 const { Router } = require('express')   
 
-const { getPosts, newPost } = require('../controllers/blog.constroller')
+const { getPosts, newPost, updatePosts, deletePosts } = require('../controllers/blog.constroller')
 const router = Router()                 
 //!{ Router } <- extraemos la clase que hace el enrutamiento 
 
 
-const users = [
-  {
-    username:'CesarDev',
-    name: 'Cesar',
-    lastname:'Mereles'
-  },
-  {
-    username:'MariaDev',
-    name: 'Maria',
-    lastname:'Podkowa'
-  },
-  {
-    username:'IgnacioDev',
-    name: 'Ignacio',
-    lastname:'Mereles'
-  }
-]
+// const users = [
+//   {
+//     username:'CesarDev',
+//     name: 'Cesar',
+//     lastname:'Mereles'
+//   },
+//   {
+//     username:'MariaDev',
+//     name: 'Maria',
+//     lastname:'Podkowa'
+//   },
+//   {
+//     username:'IgnacioDev',
+//     name: 'Ignacio',
+//     lastname:'Mereles'
+//   }
+// ]
 
 
 //todo rutas
 
-//todo servidor en escucha
-router.get('/', function(req, res) {
-   
-  res.render('home.ejs', {users})
+//============================================
+//  RUTAS PARA RENDERIZAR VISTAS 
+//============================================
+router.get('/', (req, res) => {
+  //res.render('home.ejs', {users})
+  // res.render('home.ejs')
+  res.render('index')
 
 })
 
-router.post('/nueva-publicacion', newPost)
+//============================================
+//  RUTAS PARA MANEJAR DATOS 
+//============================================
 
-//para obtener las publicaciones
-router.get('/listado-de-publicaciones', getPosts)
+router.get('/publicaciones', getPosts)
 
+router.post('/publicacion', newPost)
+
+router.put('/publicacion/:id', updatePosts)
+
+router.delete('/publicacion/:id',deletePosts)
 
 //todo PARA PODER USAR ESTE MODULO TENGO QUE EXPORTAR LOS MODULOS DE RUTAS
 module.exports = router;
